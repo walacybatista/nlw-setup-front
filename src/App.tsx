58264def73a8d9b6916d95d29ed023614d7f608a
walacybatista <walacybatistapires19@gmail.com>
2023-01-30 00:06:57 -0300

    Initial

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import { Habit } from './components/Habit'
+import './styles/global.css';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <Habit completed={3} />
+      <Habit completed={10} />
+      <Habit completed={20} />
+      <Habit completed={30} />
+    </div>
+  )
+}
+
+export default App
